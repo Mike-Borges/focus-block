42194131146f3186c5a52acdbceb75a200a7b11f
Martin Young <MartingYoung@hotmail.com>
2026-01-20 14:45:13 -0600

    adding background pics

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="page">
+    <div className="page ">
       <div className="page__content">
         <Header />
         <IdleView
